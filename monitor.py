bfb8ca37b959440520cc729f6731e006d5c56f03
Joey <FangJin98@outlook.com>
2022-08-01 19:43:19 +0800

    modify email messages

--- monitor.py
+++ monitor.py
@@ -29,7 +29,7 @@ class Record:
         self.creation_date=creation_date
 
     def __str__(self) -> str:
-        return 'Name: {} \t Creation date: {}'.format(self.name, self.creation_date)
+        return 'Name: {} \t Creation date: {}'.format(self.name, str(self.creation_date))
 
 class FileChangeHandler(FileSystemEventHandler):
     def on_moved(self, event):
@@ -51,11 +51,12 @@ def notifty_all(sender_info, receiver_list, mstp_server=USTC_MSTP_SERVER):
     for f in modified_records:
         content+=str(f)
         content+='\n'
+    content+='\n'
+    content+='To use NAS, visit https://dolomite-dancer-619.notion.site/NAS-d538f443b838426a8b465490b5dc1f43 .'
     
     email_sender=smtplib.SMTP()
     email_sender.connect(mstp_server)
     email_sender.login(sender_info[0], sender_info[1])
-
     for receiver in receiver_list:
         message =  MIMEText(content,'plain','utf-8')
         message['Subject']='Weekly NAS Movie Update'
@@ -65,19 +66,16 @@ def notifty_all(sender_info, receiver_list, mstp_server=USTC_MSTP_SERVER):
         email_sender.sendmail(
             sender_info[0],receiver, str(message)
         )
-        
     email_sender.quit()
 
 def monitor_notify(directory, sender_info, receiver_list):
     event_handler=FileChangeHandler()
-    
     observer=Observer()
     observer.schedule(event_handler, directory, recursive=True)
     observer.start()
 
     try:
         global is_modified
-        
         while True:
             time.sleep(ONE_DAY_SEC)
             week_day = datetime.today.weekday()
@@ -85,7 +83,6 @@ def monitor_notify(directory, sender_info, receiver_list):
                 notifty_all(sender_info, receiver_list)
                 modified_records.clear()
                 is_modified=False
-    
     except KeyboardInterrupt:
         observer.stop()
     
